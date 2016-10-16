860409631bbe55876532931718c41bcdd3e45683
nekonez <noscoper4ver@gmail.com>
2016-10-16 14:41:07 -0700

    lazy variables are lazy

--- garnetbot.js
+++ garnetbot.js
@@ -13,19 +13,26 @@ bot.on('botReady', () => {
 })
 
 bot.on('createdMessage', (message) => {
-  if (message.content.startsWith(`${prefix}hello`)) {
-    bot.makeMessage(message.channel_id, 'hi there!')
+
+  var msg = message.content
+  var channelID = message.channel_id
+	
+  if (msg.startsWith(`${prefix}hello`)) {
+    bot.makeMessage(channelID, 'hi there!')
   }
-  if (message.content.startsWith(`${prefix}invitelink`)) {
-    bot.makeMessage(message.channel_id, 'Thanks for asking!\nhttps://discordapp.com/oauth2/authorize?client_id=237317700043538433&scope=bot')
+  if (msg.startsWith(`${prefix}invitelink`)) {
+    bot.makeMessage(channelID, 'Thanks for asking!\nhttps://discordapp.com/oauth2/authorize?client_id=237317700043538433&scope=bot')
   }
-  if (message.content.startsWith(`${prefix}help`)) {
-    bot.makeMessage(message.channel_id, ':gem: Garnet Bot :gem: \nPrefix - g~ \nhello - hello there! \ninvitelink - sends the invitelink')
+  if (msg.startsWith(`${prefix}help`)) {
+    bot.makeMessage(channelID, ':gem: Garnet Bot :gem: \nPrefix - g~ \nhello - hello there! \ninvitelink - sends the invitelink')
   }
-  if (message.content.startsWith(`${prefix}ping`)) {
+  if (msg.startsWith(`${prefix}ping`)) {
     var realGame = games[Math.floor(Math.random() * games.length)];
-    bot.makeMessage(message.channel_id, "Pong, hey there. `" + realGame + "`")
+    bot.makeMessage(channelID, "Pong, hey there. `" + realGame + "`")
   }
+ if (msg.startsWith(`${prefix}status`)) {
+   bot.makeMessage(channelID, "I'm alive!")
+ }
 })
 
 bot.connect()
